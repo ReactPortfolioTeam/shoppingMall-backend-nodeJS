008c01f834a6467909819a30933e0d14e7e6b261
sovla <sovla@naver.com>
2021-11-19 07:27:13 +0900

    Model Freeze

--- model/Model.js
+++ model/Model.js
@@ -1,48 +1,48 @@
-export const User = {
-  userid,
-  password,
-  email,
-  name,
-  address,
-  join_date,
-  level,
-};
-
-export const Stock = {
-  stock_key,
-  product_id,
-  stock,
-  size,
-};
-export const Product = {
-  product_id,
-  product_name,
-  sub_product_name,
-  price,
-  product_desc,
-  edit_date,
-  upload_date,
-  category_id,
-};
-export const Order_product = {
-  product_id,
-  order_id,
-  quantity,
-};
-
-export const Order_info = {
-  order_id,
-  userid,
-  phone_number,
-  shipment_address,
-  order_state,
-  order_date,
-  order_price,
-};
-
-export const Auth_info = {
-  id,
-  user_id,
-  expire_date,
-  refrsh_token,
-};
+/* eslint-disable no-undef */
+module.exports = Object.freeze({
+  User: {
+    userid,
+    password,
+    email,
+    name,
+    address,
+    join_date,
+    level,
+  },
+  Stock: {
+    stock_key,
+    product_id,
+    stock,
+    size,
+  },
+  Product: {
+    product_id,
+    product_name,
+    sub_product_name,
+    price,
+    product_desc,
+    edit_date,
+    upload_date,
+    category_id,
+  },
+  Order_product: {
+    product_id,
+    order_id,
+    quantity,
+  },
+  Order_info: {
+    order_id,
+    userid,
+    phone_number,
+    shipment_address,
+    order_state,
+    order_date,
+    order_price,
+  },
+  Auth_info: {
+    id,
+    user_id,
+    expire_date,
+    refrsh_token,
+  },
+});
