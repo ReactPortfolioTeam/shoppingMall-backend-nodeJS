5b7e0780cae8f98d12b932f6bd87e6f621b16884
sovla <sovla@naver.com>
2021-11-20 22:49:25 +0900

    Fixed Model

--- model/Model.js
+++ model/Model.js
@@ -9,41 +9,39 @@ module.exports.User = {
   level: '',
 };
 
-//   ,
-//   Stock: {
-//     stock_key,
-//     product_id,
-//     stock,
-//     size,
-//   },
-//   Product: {
-//     product_id,
-//     product_name,
-//     sub_product_name,
-//     price,
-//     product_desc,
-//     edit_date,
-//     upload_date,
-//     category_id,
-//   },
-//   Order_product: {
-//     product_id,
-//     order_id,
-//     quantity,
-//   },
-//   Order_info: {
-//     order_id,
-//     userid,
-//     phone_number,
-//     shipment_address,
-//     order_state,
-//     order_date,
-//     order_price,
-//   },
-//   Auth_info: {
-//     id,
-//     user_id,
-//     expire_date,
-//     refrsh_token,
-//   },
-// };
+module.exports.Stock = {
+  stock_key: '',
+  product_id: '',
+  stock: '',
+  size: '',
+};
+module.exports.Product = {
+  product_id: '',
+  product_name: '',
+  sub_product_name: '',
+  price: '',
+  product_desc: '',
+  edit_date: '',
+  upload_date: '',
+  category_id: '',
+};
+module.exports.Order_product = {
+  product_id: '',
+  order_id: '',
+  quantity: '',
+};
+module.exports.Order_info = {
+  order_id: '',
+  userid: '',
+  phone_number: '',
+  shipment_address: '',
+  order_state: '',
+  order_date: '',
+  order_price: '',
+};
+module.exports.Auth_info = {
+  id: '',
+  user_id: '',
+  expire_date: '',
+  refrsh_token: '',
+};
--- src/util/objectChangeArray.js
+++ src/util/objectChangeArray.js
@@ -1,4 +1,5 @@
 module.exports = function objectChangeArray(Object) {
+  // by.Junhan 오브젝트 순서대로 배열에 값만 넣어주는 메소드  11.20
   let result = [];
   for (const item in Object) {
     result.push(Object[item]);
