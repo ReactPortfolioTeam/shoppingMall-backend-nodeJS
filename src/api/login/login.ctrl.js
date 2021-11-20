b208573ee1415da3fbea84898a846657739b3ed1
dhchun1203 <dhchun1203@gmail.com>
2021-11-20 21:39:15 +0900

    로그인/로그아웃api

--- src/api/login/login.ctrl.js
+++ src/api/login/login.ctrl.js
@@ -1,12 +1,35 @@
-const Model = require('../../../model/Model');
+const mysql = require('mysql2');
+const config = require('../../../config/config.json');
 
-const UserInfo = Model.User;
-UserInfo.userid = 'dhchun1203';
+const connection = mysql.createConnection({
+  host: config.dev.host,
+  user: config.dev.user,
+  port: config.dev.port,
+  password: config.dev.password,
+  database: config.dev.database,
+});
 
-exports.write = (ctx) => {
-  //
-};
-
-exports.list = (ctx) => {
-  ctx.body = UserInfo.userid;
+exports.login = (ctx) => {
+  const { userid, password } = ctx.request.body;
+  if (userid && password) {
+    if (userid && password) {
+      connection.query(
+        `SELECT * FROM user WHERE userid = "${userid}" AND password = "${password}"`,
+        [userid, password],
+        function (error, results, fields) {
+          if (error) throw error;
+          if (results.length > 0) {
+            // ctx.request.session.loggedin = true; jwt 토큰 적용? 로그인과 로그아웃 상태를 구분하는?
+            // ctx.request.session.userid = userid;
+            ctx.response.redirect('/'); // 메인페이지로 이동
+            console.log(results);
+          } else {
+            console.log('로그인 정보가 일치하지 않습니다.');
+          }
+        },
+      );
+    } else {
+      console.log('아이디와 비밀번호를 입력해주세요.');
+    }
+  }
 };
--- src/index.js
+++ src/index.js
@@ -6,7 +6,28 @@ const router = new Router();
 
 const api = require('./api');
 
-router.use('/login', api.routes());
+const mysql = require('mysql2');
+const config = require('../config/config.json');
+
+const connection = mysql.createConnection({
+  host: config.dev.host,
+  user: config.dev.user,
+  port: config.dev.port,
+  password: config.dev.password,
+  database: config.dev.database,
+});
+
+connection.connect(); // 연결
+
+connection.query('select * from user', (err, rows, fields) => {
+  if (err) throw err;
+  console.log('User info is ...');
+  console.table(rows);
+});
+
+connection.end();
+
+router.use('/api', api.routes());
 
 app.use(bodyParser());
 
