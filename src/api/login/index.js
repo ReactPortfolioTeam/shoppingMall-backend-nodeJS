6c58c93327a88cd66d7529a3de969c485d0e4963
dhchun1203 <dhchun1203@gmail.com>
2021-11-21 16:55:43 +0900

    login.ctrl fix & login router fix

--- src/api/login/index.js
+++ src/api/login/index.js
@@ -2,8 +2,5 @@ const Router = require('koa-router');
 const login = new Router();
 const loginCtrl = require('./login.ctrl');
 
-login.get('/', (ctx) => {
-  ctx.body = `GET ${ctx.request.path}`;
-});
-login.post('/localLogin', loginCtrl.login);
+login.post('/', loginCtrl.login);
 module.exports = login;
