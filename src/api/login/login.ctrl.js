9bae1f3b215a9a148b5557bc6b7708603eceb59c
dhchun1203 <dhchun1203@gmail.com>
2021-11-22 18:24:42 +0900

    login.ctrl async/await 적용

--- src/api/login/login.ctrl.js
+++ src/api/login/login.ctrl.js
@@ -1,38 +1,45 @@
-const mysql = require('mysql2');
-const config = require('../../../config/config.json');
 const getConnection = require('../mysql');
-
-const connection = mysql.createConnection({
+const config = require('../../../config/config.json');
+const pool = require('mysql2/promise').createPool({
   host: config.dev.host,
   user: config.dev.user,
   port: config.dev.port,
   password: config.dev.password,
   database: config.dev.database,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
 const sql = 'SELECT * FROM user WHERE userid = ? AND password = ?';
 
-exports.login = (ctx) => {
+exports.login = async (ctx) => {
   const { userid, password } = ctx.request.body;
-
+  let data;
+  try {
+    data = await pool.query(sql, [userid, password]);
+    ctx.status = 202;
+  } catch (error) {
+    console.log(error);
+  }
   if (userid && password) {
-    getConnection((conn) => {
-      conn.query(sql, [userid, password], (error, results, fields) => {
-        if (error) throw error;
-        if (results.length > 0) {
-          // ctx.request.session.loggedin = true; jwt 토큰 적용? 로그인과 로그아웃 상태를 구분하는?
-          // ctx.request.session.userid = userid;
-          ctx.status = 201;
-          ctx.response.redirect('/'); // 메인페이지로 이동
-          console.log(results);
-          ctx.body = userid + '님의 로그인에 성공하였습니다.';
-        } else {
-          console.log('로그인 정보가 일치하지 않습니다.');
-        }
-      });
-    });
+    if (data[0][0]) {
+      ctx.body = {
+        data: data[0][0],
+        status: ctx.status,
+      };
+    } else {
+      ctx.status = 404;
+      ctx.body = {
+        message: '로그인 정보가 일치하지 않습니다.',
+        status: ctx.status,
+      };
+    }
   } else {
-    console.log('아이디와 비밀번호를 입력해주세요.');
-    ctx.body = '아이디와 비밀번호를 입력해주세요.';
+    ctx.status = 404;
+    ctx.body = {
+      message: '아이디와 비밀번호를 입력해주세요',
+      status: ctx.status,
+    };
   }
 };
