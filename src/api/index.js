5fe9d3ab653c8382a6e5343a415dacf8f6248b5a
dhchun1203 <dhchun1203@gmail.com>
2021-11-24 19:45:14 +0900

    userInfo 회원정보 조회 및 수정 기능 추가

--- src/api/index.js
+++ src/api/index.js
@@ -3,11 +3,14 @@ const login = require('./login');
 
 const signup = require('./signup');
 const logout = require('./logout');
+const userInfo = require('./userInfo');
 
 const api = new Router();
 
 api.use('/login', login.routes());
 api.use('/logout', logout.routes());
+
+api.use('/userInfo', userInfo.routes());
 
 api.use('/signup', signup.routes());
 
